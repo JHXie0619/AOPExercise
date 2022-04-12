0458a8f5e1d3955296fc4c59bc7fbd9116c8dc9d
HITRUST\frankzhang <frankzhang@hitrust.com.tw>
2022-04-12 22:54:51 +0800

    First commit

---
+++ src/test/java/tw/com/hitrust/AopExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.hitrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
